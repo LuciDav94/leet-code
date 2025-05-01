481827690306abfbbcd633da6bcd404f1423fe27
Lucian Davidescu <ldavidescu@maretha.io>
2025-05-01 10:51:24 +0300

    added 1768 problem

---
+++ 1768-merge-strings-alternately/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String word1 = "ab", word2 = "pqrsdsss";
+        System.out.println(mergeAlternately(word1, word2));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        String result = "";
+        int i=0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result += word1.charAt(i);
+            }
+            if( i < word2.length()){
+                result += word2.charAt(i);
+            }
+            i++;
+        }
+        return result;
+    }
+}
